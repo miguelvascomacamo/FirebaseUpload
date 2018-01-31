7cf1c82200858074bfd5b1a7e6f36f9928e607ba
Miguel <miguelvascomacamo@gmail.com>
2018-02-01 00:55:00 +0200

    Criando a classe ImageAdaptar

---
+++ app/src/main/java/com/firebaseupload/miguel/firebaseupload/ImageAdaptar.java
@@ -0,0 +1,10 @@
+package com.firebaseupload.miguel.firebaseupload;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by miguel on 2/1/18.
+ */
+
+public class ImageAdaptar extends RecyclerView.Adapter {
+}
